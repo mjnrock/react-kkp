ceea5f0609aea03b1320d9f65598bc4bc9136ab1
Matt Nawrocki <mjnrock@umich.edu>
2019-11-13 20:27:00 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { Provider } from "mobx-react";
+import UiStore from "./stores/UiStore";
+
+const Root = (
+    <Provider UiStore={ UiStore }>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(Root, document.getElementById("root"));
