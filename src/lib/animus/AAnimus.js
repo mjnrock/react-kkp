86e456ac1bdaa70fe326f9f043e6fb0b7ac556d1
Matt Nawrocki <mjnrock@umich.edu>
2019-11-20 22:48:28 -0500

    init

--- src/lib/animus/AAnimus.js
+++ src/lib/animus/AAnimus.js
@@ -4,12 +4,21 @@ export default class AAnimus extends AState {
     constructor(events = {}, state = {}) {
         super(state);
 
+        this.uuid = AAnimus.GenerateUUID();
         this.events = events;
         this.listeners = {};
 
         if("init" in this.events) {
             this.trigger("init");
         }
+    }
+
+    static GenerateUUID() {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+            // eslint-disable-next-line
+            var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
 
     listen(name, listener) {
@@ -21,7 +30,14 @@ export default class AAnimus extends AState {
 
         return this;
     }
-    unlisten(name, listener) {
+    //? Removes all listeners for an event
+    unlisten(name) {
+        delete this.listeners[ name ];
+
+        return this;
+    }
+    //? Removes the passed @listener from the event
+    decouple(name, listener) {
         if(Array.isArray(this.listeners[ name ])) {
             for(let i in this.listeners[ name ]) {
                 let list = this.listeners[ name ][ i ];
